b5e943d567a17c83fb891091f625f92510422849
NurbekRajabov <nurbek0553@gmail.com>
2021-12-09 23:51:55 +0500

    update program cs

--- Program.cs
+++ Program.cs
@@ -6,7 +6,7 @@ namespace console
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("qale");
+            Console.WriteLine("Nima gapla");
         }
     }
 }
