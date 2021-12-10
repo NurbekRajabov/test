64cc16378d2169d612791df2a6aaad994e476bad
NurbekRajabov <94293364+NurbekRajabov@users.noreply.github.com>
2021-12-10 17:31:58 +0500

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -6,7 +6,7 @@ namespace console
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Main branch ");
+            Console.WriteLine("Hello from remote");
 
         }
     }
